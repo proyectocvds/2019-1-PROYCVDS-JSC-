fed34c9b13e27c38fc9dce209b283b8890d194c2
camilavieda04 <sarah.vieda@mail.escuelaing.edu.co>
2019-04-09 17:07:42 -0500

    tablas

---
+++ tablas.sql
@@ -0,0 +1,30 @@
+﻿
+--Tablas
+
+
+CREATE TABLE public.elemento (
+	id varchar NOT NULL,
+	disponible bool NOT NULL,
+	tipo varchar NOT NULL,
+	CONSTRAINT elemento_pk PRIMARY KEY (id)
+);
+
+
+CREATE TABLE public.equipo (
+	id varchar NOT NULL,
+	disponible bool NOT NULL,
+	CONSTRAINT equipo_pk PRIMARY KEY (id)
+);
+
+
+
+CREATE TABLE public.usuario (
+	username varchar(50) NOT NULL,
+	estado bool NOT NULL,
+	correo varchar NOT NULL,
+	contrasena varchar NOT NULL,
+	informacion varchar NULL,
+	CONSTRAINT usuario_pk PRIMARY KEY (username),
+	CONSTRAINT usuario_un UNIQUE (correo)
+);
+
