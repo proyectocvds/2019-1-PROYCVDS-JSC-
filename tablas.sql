c120c80448d24b2823c9119a479c6a11c7dbb5e7
camilavieda04 <sarah.vieda@mail.escuelaing.edu.co>
2019-04-10 10:53:42 -0500

    Tabla novedad

--- tablas.sql
+++ tablas.sql
@@ -1,14 +1,24 @@
-﻿
 --Tablas
 
 
-CREATE TABLE public.elemento (
+--Tabla novedad
+
+CREATE TABLE public.novedad (
 	id varchar NOT NULL,
-	disponible bool NOT NULL,
-	tipo varchar NOT NULL,
-	CONSTRAINT elemento_pk PRIMARY KEY (id)
+	"fecha " date NOT NULL,
+	"titulo " varchar NOT NULL,
+	usuario varchar NOT NULL,
+	detalle varchar NOT NULL,
+	equipo varchar NOT NULL,
+	elemento varchar NOT NULL,
+	CONSTRAINT novedad_pk PRIMARY KEY (id),
+	CONSTRAINT novedad_elemento_fk FOREIGN KEY (id) REFERENCES elemento(id),
+	CONSTRAINT novedad_equipo_fk FOREIGN KEY (id) REFERENCES equipo(id),
+	CONSTRAINT novedad_usuario_fk FOREIGN KEY (id) REFERENCES usuario(username)
 );
 
+
+--Tabla equipo
 
 CREATE TABLE public.equipo (
 	id varchar NOT NULL,
@@ -18,13 +28,31 @@ CREATE TABLE public.equipo (
 
 
 
+--Tabla elemento
+
+CREATE TABLE public.elemento (
+	id varchar NOT NULL,
+	disponible bool NOT NULL,
+	tipo varchar NOT NULL,
+	CONSTRAINT elemento_pk PRIMARY KEY (id)
+);
+
+
+--Tabla usuario
+
+
 CREATE TABLE public.usuario (
-	username varchar(50) NOT NULL,
 	estado bool NOT NULL,
 	correo varchar NOT NULL,
 	contrasena varchar NOT NULL,
-	informacion varchar NULL,
+	informacion varchar NOT NULL,
+	username varchar NOT NULL,
 	CONSTRAINT usuario_pk PRIMARY KEY (username),
 	CONSTRAINT usuario_un UNIQUE (correo)
 );
+
+
+
+
+
 
